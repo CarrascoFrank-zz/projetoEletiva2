fb7cc5e1dba8e55e5315dac92dce77790e97766b
Frank Carrasco <carrasco.frank10@gmail.com>
2017-11-30 15:06:40 -0200

    adicionando a activity de cadastro e a chamada dela.

--- app/src/main/java/com/example/frankgalvez/appcurriculo/MainActivity.java
+++ app/src/main/java/com/example/frankgalvez/appcurriculo/MainActivity.java
@@ -13,9 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Button buttonCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +48,17 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
+        //Evento do botao cadastrar
 
+        buttonCadastrar = (Button) findViewById(R.id.id_buttonCadastrar);
+
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
