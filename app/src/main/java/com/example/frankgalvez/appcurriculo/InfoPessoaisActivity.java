165be90fcd84846335ebbe97259b4ea5b554ab36
Frank Carrasco <carrasco.frank10@gmail.com>
2017-12-12 16:46:24 -0200

    implementando SQLite

--- app/src/main/java/com/example/frankgalvez/appcurriculo/InfoPessoaisActivity.java
+++ app/src/main/java/com/example/frankgalvez/appcurriculo/InfoPessoaisActivity.java
@@ -1,5 +1,7 @@
 package com.example.frankgalvez.appcurriculo;
 
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class InfoPessoaisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_pessoais);
+
+        SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
+
+        //TABELA
+        bancoDados.execSQL("CREATE TABLE IF NOT EXISTS dadospessoais(nome varchar, idade INT(3) ) ");
+
+
     }
 }
